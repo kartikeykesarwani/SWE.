7ef1b31c140e9bb26826d90a3dc888d246675cac
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2021-02-20 02:38:34 +0530

    added alert reducer

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,32 @@
 import React, { Fragment } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'; 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
-
-
+import Alert from './components/layout/Alert';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
 
 import './App.css';
 
+const App = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar />
+        <Route exact path='/' component={Landing} />
+        <section className='container'>
+          <Alert />
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+          </Switch>
+        </section>
+      </Fragment>
+    </BrowserRouter>
+  </Provider>
+);
 
-
-const App = () => 
-<BrowserRouter>
-    <Fragment>
-      <Navbar/>
-      <Route exact path='/' component = {Landing}/>
-       <section className="container">
-         <Switch>
-           <Route exact path="/register" component={Register} />
-           <Route exact path="/login" component={Login}/>
-         </Switch>
-       </section>
-    </Fragment>
-</BrowserRouter>
-    
-  
 export default App;
