88cf7dbd9323406e65ab0a3413cc76e438a3289b
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2021-07-25 20:36:16 +0530

    prepare for deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 //Connect Database
 connectDB();
@@ -8,7 +9,7 @@ connectDB();
 const PORT = process.env.PORT || 5000;
 
 //init Middleware
-app.use(express.json({extended: false}));
+app.use(express.json({ extended: false }));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
@@ -17,5 +18,11 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
-app.get('/', (req, res) => res.send('API Running'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
